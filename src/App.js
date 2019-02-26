125b73fd86b6821e5df777da2f3be6a258a49e73
carlos <carlos.castaneda.gutierrez@gmail.com>
2019-02-26 10:11:28 -0500

    Fix the URL of the Weather API

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SkappBar from './components/SkappBar/SkappBar';
 import styles from './Skapp.module.scss';
 import SkappHome from './components/SkappHome/SkappHome';
 import SkappForecast from './components/SkappForecast/SkappForecast';
-import { getForecast } from './utils/api';
+import { getDetail, getForecast } from './utils/api';
 
 class App extends Component {
   citySubmit = async(city) => {
